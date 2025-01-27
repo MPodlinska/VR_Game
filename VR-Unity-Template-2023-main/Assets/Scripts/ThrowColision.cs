101478878fd09fafb51b17b0233977425fa1c091
Magdalena Podlińska <s184441@student.pg.edu.pl>
2025-01-27 21:34:46 +0100

    Throw Finale

--- VR-Unity-Template-2023-main/Assets/Scripts/ThrowColision.cs
+++ VR-Unity-Template-2023-main/Assets/Scripts/ThrowColision.cs
@@ -1,30 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class ThrowColision : MonoBehaviour
+public class ThrowCollision : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    private bool hasHitTarget = false;  
+    private Vector3 originalPosition;  
+    private bool isThrown = false;   
+    private XRGrabInteractable grabInteractable;  
+    private Rigidbody rb;
+
+    void Start()
     {
-        if(other.gameObject.tag == "Throw_Aim")
-        {
-            print("ENTER");
-        }
+        originalPosition = transform.position;  
+        grabInteractable = GetComponent<XRGrabInteractable>();  
+        rb = GetComponent<Rigidbody>();  
+
+        
+        grabInteractable.onSelectExited.AddListener(OnThrow);
+
+        rb.isKinematic = true; 
     }
 
-    void OnTriggerStay(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Throw_Aim")
+        if (other.CompareTag("Throw_Aim"))
         {
-            print("COLISSION");
+            hasHitTarget = true;  
+            print($"{gameObject.name} hit the target!");
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Throw_Aim")
+        if (other.CompareTag("Throw_Aim"))
         {
-            print("EXIT");
+            hasHitTarget = false; 
+            print($"{gameObject.name} exited the target zone.");
         }
+    }
+
+    private void OnThrow(XRBaseInteractor interactor)
+    {
+        isThrown = true;
+        print($"{gameObject.name} was thrown!");
+
+        rb.isKinematic = false;
+
+        StartCoroutine(CheckReturn());
+    }
+
+    private IEnumerator CheckReturn()
+    {
+        yield return new WaitForSeconds(2f);
+
+        while (rb.velocity.magnitude > 0.1f)
+        {
+            yield return null;  
+        }
+
+        if (!hasHitTarget)
+        {
+            ResetObject();
+        }
+    }
+
+    private void ResetObject()
+    {
+        print($"{gameObject.name} is resetting...");
+
+        rb.isKinematic = true;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        transform.position = originalPosition;
+        transform.rotation = Quaternion.identity;
+
+        isThrown = false;
     }
 }
